361e08bcd67e309e972bf0f1f78e3928813b7258
Marco Henrique Nunes Pereira <marcohnp@hotmail.com>
2021-11-24 17:00:08 -0300

    Projeto enviado ao GitHub.

---
+++ src/main/java/com/example/dev2projetobackend/Dev2ProjetoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dev2projetobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev2ProjetoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dev2ProjetoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dev2projetobackend/Dev2ProjetoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev2projetobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev2ProjetoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
