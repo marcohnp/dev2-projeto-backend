99720825882bc464ddf9573ff5ca223d75f928a9
Marco Henrique Nunes Pereira <marcohnp@hotmail.com>
2022-01-24 15:38:14 -0300

    Ajustes entidade item.

--- src/main/java/com/example/dev2projetobackend/model/Item.java
+++ src/main/java/com/example/dev2projetobackend/model/Item.java
@@ -25,11 +25,11 @@ public class Item {
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date data;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
 
